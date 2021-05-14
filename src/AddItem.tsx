c188eeab0fd0af1609c46a779d8846d40f8895b4
Aditya Chukka <adityaharish626@gmail.com>
2021-05-13 23:37:19 -0400

    Add: Form to read and write todo list (#4)
    - Define interface item in todo list
    - Add ToDoList Component to present todolist
       - Use a tabular format
       - Show items sorted by priority
    - Add AddItem Component to write an item
       - Validate item before saving
       - Call parent component upon submit
    - App renders todo list based on chagnes
       - Reads newitem for AddItem form
       - Validates for duplicates
       - Update states, rerender TodoList Form
    - Add tests for App, TodoList and AddItem components

---
+++ src/AddItem.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Item } from "./ToDoList";
+
+const isValid = (item: Item): boolean => {
+  return item.task !== "" && item.priority !== -1;
+};
+
+class AddItem extends React.Component<{ addItem: any }, Item> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      task: "",
+      priority: -1,
+    };
+    this.setTask = this.setTask.bind(this);
+    this.setPriority = this.setPriority.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  setTask(evt: any) {
+    this.setState({
+      task: evt.target.value,
+    });
+  }
+
+  setPriority(evt: any) {
+    this.setState({
+      priority: parseInt(evt.target.value),
+    });
+  }
+
+  addItem(evt: any) {
+    const item = this.state;
+    if (isValid(item)) {
+      this.props.addItem(item);
+    }
+
+    this.setState({
+      task: "",
+      priority: -1,
+    });
+  }
+
+  render() {
+    return (
+      <table>
+        <tbody>
+          <tr key={""}>
+            <td>Task:</td>
+            <td>
+              <input
+                id="task"
+                type="text"
+                placeholder="Enter task here"
+                onChange={this.setTask}
+              />
+            </td>
+            <td>Priority:</td>
+            <td>
+              <input
+                id="prioity"
+                type="text"
+                placeholder="Enter priority here"
+                onChange={this.setPriority}
+              />
+            </td>
+            <td>
+              <input id="submit" type="submit" onClick={this.addItem} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default AddItem;
